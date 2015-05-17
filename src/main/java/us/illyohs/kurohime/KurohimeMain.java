88f7a9be1d3554df58e1c0bc9916e33dc661b6e0
Anthony Anderson <lordillyohs@gmail.com>
2015-05-17 03:56:56 -0400

    Dammit .-.
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

--- src/main/java/us/illyohs/kurohime/KurohimeMain.java
+++ src/main/java/us/illyohs/kurohime/KurohimeMain.java
@@ -1,6 +1,6 @@
 public class KurohimeMain {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("I am being written at 3AM woo!!!");
     }
 
